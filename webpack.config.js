0975b076222b52e8570bce6e64bf8e74912326a8
Софья Нильга <nlgs1337@gmail.com>
2021-11-15 12:55:03 +0200

    v 1

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
         {
           test: /\.vue$/,
           loader: 'vue-loader'
+        },
+        {
+          test: /.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+          ]
         }
       ]
     },
