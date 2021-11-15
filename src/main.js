3a6e1da2b3e0463558e069274bb30cc871a8cbcf
Софья Нильга <nlgs1337@gmail.com>
2021-11-15 13:22:55 +0200

    weather v1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
-import App from './components/App.vue'
+import VueRouter from 'vue-router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-    
-    Vue.use(VueAxios, axios)
 
+import AllWeather from './components/AllWeather.vue'
+import Weather from './components/Weather.vue'
+import App from './components/App.vue'
+    
+const routes = [
+    {path: '/', component: AllWeather},
+    {path: '/weather-info/:city', component: Weather, props: true}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+    Vue.use( VueAxios, axios)
+    Vue.use(VueRouter)
 
 new Vue({
    render: h => h(App),
    el: '#app',
+   router
 })
